ddb4f07d8cefa47f5245e7ad38f70954467bdf45
Etoria <ccicharity@gmail.com>
2025-03-10 20:05:05 -0700

    Step 1: Database Migrations

---
+++ .history/backend/db/migrations/20250310023937-create-spot-image_20250310182133.js
@@ -0,0 +1,56 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+
+    // Define the options object for production
+    const options = {};
+
+    if (process.env.NODE_ENV === 'production') {
+      options.schema = 'public'; // Set schema if in production
+    }
+
+    await queryInterface.createTable('SpotImages', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      spotId: {
+        allowNull: false,
+        references: {
+          model: 'Spots',
+          key: 'id'
+        },
+        onDelete: 'CASCADE',
+        type: Sequelize.INTEGER
+      },
+      url: {
+        allowNull: false
+        type: Sequelize.STRING
+      },
+      preview: {
+        allowNull: false,
+        defaultValue: false
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+        type: Sequelize.DATE
+      }
+    }, options);
+  },
+  async down(queryInterface, Sequelize) {
+    
+    await queryInterface.dropTable('SpotImages', options);
+  }
+};
