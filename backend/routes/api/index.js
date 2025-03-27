8b18cb3ee76122f2a361e432ea8707486ad41fa8
Etoria <ccicharity@gmail.com>
2025-03-26 20:33:02 -0700

    Completed Connect the Spots Router to the Main API Router

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,12 +1,27 @@
-const router = require('express').Router();
-const sessionRouter = require('./session.js');
-const usersRouter = require('./users.js');
-const { restoreUser } = require("../../utils/auth.js");
 const { setTokenCookie } = require('../../utils/auth.js');
 const { User } = require('../../db/models');
 const { requireAuth } = require('../../utils/auth.js');
 
+//1. Import express and create router
+const router = require('express').Router();
+
+//2. Import route modules:
+//- sessionRouter
+const sessionRouter = require('./session.js');
+
+//- usersRouter
+const usersRouter = require('./users.js');
+
+//- spotsRouter
+const spotsRouter = require('./spots');
+
+//3. Import restoreUser middleware
+const { restoreUser } = require("../../utils/auth.js");
+
+//4. Apply restoreUser middleware to set req.user
 router.use(restoreUser);
+
+//5. Connect route modules:
 
 router.use('/session', sessionRouter);
 
@@ -41,4 +56,5 @@ router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
 });
 
+//6. Export router
 module.exports = router;
