e7706755907a68b40443fb233a7fc16cf086d10f
Etoria <ccicharity@gmail.com>
2025-04-21 20:47:14 -0700

    Refactored migrations

--- backend/db/migrations/20250403011055-add-first-last-name-to-users.js
+++ backend/db/migrations/20250403011055-add-first-last-name-to-users.js
@@ -2,21 +2,30 @@
 
 let options = {};
 if (process.env.NODE_ENV === 'production') {
-  options.schema = process.env.SCHEMA;
+  options.schema = process.env.SCHEMA; // define your schema
 }
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('Users', 'firstName', {
-      type: Sequelize.STRING,
-      allowNull: false,
-      defaultValue: 'First'
-    });
-    await queryInterface.addColumn('Users', 'lastName', {
-      type: Sequelize.STRING,
-      allowNull: false,
-      defaultValue: 'Last'
-    });
+    await queryInterface.addColumn(
+      { tableName: 'Users', ...options },
+      'firstName',
+      {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: 'First'
+      }
+    );
+
+    await queryInterface.addColumn(
+      { tableName: 'Users', ...options },
+      'lastName',
+      {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: 'Last'
+      }
+    );
   },
 
   async down(queryInterface, Sequelize) {
@@ -24,7 +33,8 @@ module.exports = {
     if (process.env.NODE_ENV === 'production') {
       options.schema = process.env.SCHEMA;
     }
-    await queryInterface.removeColumn('Users', 'firstName');
-    await queryInterface.removeColumn('Users', 'lastName');
+
+    await queryInterface.removeColumn({ tableName: 'Users', ...options }, 'firstName');
+    await queryInterface.removeColumn({ tableName: 'Users', ...options }, 'lastName');
   }
 };
