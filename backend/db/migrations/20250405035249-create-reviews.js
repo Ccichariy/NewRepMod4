a3e00cd8a233c2a7f0a76859a31e6cf8b24f6bf2
Etoria <ccicharity@gmail.com>
2025-04-21 20:53:37 -0700

    Added missing options to migrations

--- backend/db/migrations/20250405035249-create-reviews.js
+++ backend/db/migrations/20250405035249-create-reviews.js
@@ -1,5 +1,10 @@
 'use strict';
 
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Reviews', {
@@ -47,6 +52,10 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
+    const options = {}
+    if (process.env.NODE_ENV === 'production') {
+      options.schema = process.env.SCHEMA;
+    }
     await queryInterface.dropTable('Reviews');
   }
 };
--- backend/db/migrations/20250412091041-create-review-image.js
+++ backend/db/migrations/20250412091041-create-review-image.js
@@ -1,5 +1,10 @@
 'use strict';
 
+const options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -34,6 +39,10 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
+    const options = {};
+    if (process.env.NODE_ENV === 'production') {
+      options.schema = process.env.SCHEMA;
+    }
     await queryInterface.dropTable('ReviewImages');
   }
 };
