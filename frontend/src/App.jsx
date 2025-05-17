d1bdea0df4380a5e9edca1f4210ea6dc7460a9b3
Etoria Cheeks <ccicharity@gmail.com>
2025-05-17 16:16:32 -0700

    Phases 3-5

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,30 @@
+// frontend/src/App.jsx
+
 import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
-import LoginFormPage from './components/LoginFormPage';
+// import LoginFormPage from './components/LoginFormModal/LoginFormModal.jsx';
+import SignupFormPage from './components/SignupFormModal/SignupFormModal.jsx';
+import Navigation from './components/Navigation';
 import * as sessionActions from './store/session';
+
+function Layout() {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
+  }, [dispatch]);
+
+  return (
+    <>
+      {isLoaded && <Outlet />}
+    </>
+  );
+}
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <h1>Welcome!</h1>
-  },
-  {
-    path: '/login',
-    element: <LoginFormPage />
-  },
-
-{
     element: <Layout />,
     children: [
       {
@@ -24,34 +34,76 @@ const router = createBrowserRouter([
       {
         path: '/login',
         element: <LoginFormPage />
+      },
+      {
+        path: '/signup',
+        element: <SignupFormPage />
       }
     ]
   }
-  ]);
-
-function App() {
-  return <h1> Hello from App </h1>;
-}
-
-function Layout() {
-  const dispatch = useDispatch();
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  useEffect(() => {
-    dispatch(sessionActions.restoreUser()).then(() => {
-      setIsLoaded(true)
-    });
-  }, [dispatch]);
-
-  return (
-    <>
-      {isLoaded && <Outlet />}
-    </>
-  );
-};
+]);
 
 function App() {
   return <RouterProvider router={router} />;
 }
 
 export default App;
+
+
+// import { useState, useEffect } from 'react';
+// import { useDispatch } from 'react-redux';
+// import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+// import LoginFormPage from './components/LoginFormPage';
+// import * as sessionActions from './store/session';
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <h1>Welcome!</h1>
+//   },
+//   {
+//     path: '/login',
+//     element: <LoginFormPage />
+//   },
+
+// {
+//     element: <Layout />,
+//     children: [
+//       {
+//         path: '/',
+//         element: <h1>Welcome!</h1>
+//       },
+//       {
+//         path: '/login',
+//         element: <LoginFormPage />
+//       }
+//     ]
+//   }
+//   ]);
+
+// function App() {
+//   return <h1> Hello from App </h1>;
+// }
+
+// function Layout() {
+//   const dispatch = useDispatch();
+//   const [isLoaded, setIsLoaded] = useState(false);
+
+//   useEffect(() => {
+//     dispatch(sessionActions.restoreUser()).then(() => {
+//       setIsLoaded(true)
+//     });
+//   }, [dispatch]);
+
+//   return (
+//     <>
+//       {isLoaded && <Outlet />}
+//     </>
+//   );
+// };
+
+// function App() {
+//   return <RouterProvider router={router} />;
+// }
+
+// export default App;
